2e5f0c13bddceb5ea6531037ac98650fc3eb1f56
Aledj2 <Aledjones@nhs.net>
2019-12-09 11:18:18 +0000

    Fix wscleaner logfile name

--- wscleaner/wscleaner_command.sh
+++ wscleaner/wscleaner_command.sh
@@ -5,7 +5,7 @@ eval "$(conda shell.bash hook)" # Add conda environment to system path
 conda activate wscleaner
 
 # Set varianbles
-logfile="/home/mokaguys/Documents/automate_demultiplexing_logfiles/wscleaner_logs/wscleaner_$(date -d now +%y%m%d).log"
+logfile="/home/mokaguys/Documents/automate_demultiplexing_logfiles/wscleaner_logs/$(date -d now +%y%m%d)_wscleaner.log"
 runfolders="/media/data3/share"
 
 # Execute
