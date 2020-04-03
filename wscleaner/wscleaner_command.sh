38978a1d89fc284e288ac58ed78fc0090bf3b1df
Aledj2 <Aledjones@nhs.net>
2020-04-03 14:52:19 +0100

    Update wscleaner command for crontab to use proper environment

--- wscleaner/wscleaner_command.sh
+++ wscleaner/wscleaner_command.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Activate wscleaner environment
-eval "$(conda shell.bash hook)" # Add conda environment to system path
+eval "$(/usr/local/bin/miniconda3/bin/conda shell.bash hook)" # Add conda environment to system path
 conda activate wscleaner
 
-# Set varianbles
+# Set variables
 logfile="/home/mokaguys/Documents/automate_demultiplexing_logfiles/wscleaner_logs/$(date -d now +%y%m%d)_wscleaner.log"
 runfolders="/media/data3/share"
 
